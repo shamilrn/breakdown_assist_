6e9e44320cd1a4625b895785943ac8b4a372082c
shamilrn <shamilrn10@gmail.com>
2024-03-15 12:33:19 +0530

    second

---
+++ lib/admin/admin_login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Admin_login extends StatefulWidget {
+  const Admin_login({super.key});
+
+  @override
+  State<Admin_login> createState() => _Admin_loginState();
+}
+
+class _Admin_loginState extends State<Admin_login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'admin/admin_login.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Admin_login(),
+    );
   }
 }
