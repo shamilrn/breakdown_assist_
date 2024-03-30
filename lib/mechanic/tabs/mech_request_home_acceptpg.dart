cd70cfbb32d689e290cc48ee48c7a5910b02e2d8
shamilrn <shamilrn10@gmail.com>
2024-03-30 12:21:09 +0530

    10th

--- lib/mechanic/tabs/mech_request_home_acceptpg.dart
+++ lib/mechanic/tabs/mech_request_home_acceptpg.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Mech_AcceptPg extends StatefulWidget {
@@ -8,66 +9,83 @@ class Mech_AcceptPg extends StatefulWidget {
 }
 
 class _Mech_AcceptPgState extends State<Mech_AcceptPg> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-          separatorBuilder: (context, index)=> Divider(thickness: 5, color: Colors.white,
-          ),
-          itemCount: 3,
-        itemBuilder: (BuildContext context, int index){
-            return Container(
-              height: 100,
-              width: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 40,
-                        backgroundImage: AssetImage("assets/image/profile.jpg"),
-                      ),
-                      Text("Name",style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                  Spacer(),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-
-                      Text("Fuel leaking",style: TextStyle(fontSize: 13)),
-                      Text("Date",style: TextStyle(fontSize: 13)),
-                      Text("Time",style: TextStyle(fontSize: 13)),
-                      Text("Place",style: TextStyle(fontSize: 13)),
-                    ],
-                  ),
-                  Spacer(),
-                  Container(
-                    height: 30,
-                    width: 130,
-                    decoration: BoxDecoration(
-                        color: Colors.green,
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-
-                    child: Center(child: Text("Payment Success")),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                ],
-              ),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance.collection('mechreq').where('status', isEqualTo: 1).get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error:${snapshot.error}"),
+            );
+          }
+          final detail = snapshot.data?.docs ?? [];
+
+          return  ListView.separated(
+            separatorBuilder: (context, index)=> Divider(thickness: 5, color: Colors.white,
+            ),
+            itemCount: detail.length,
+            itemBuilder: (BuildContext context, int index){
+              return Container(
+                height: 100,
+                width: 200,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 40,
+                          backgroundImage: AssetImage("assets/image/profile.jpg"),
+                        ),
+                        Text(detail[index]['username'],style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    Spacer(),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+
+                        Text(detail[index]['service'],style: TextStyle(fontSize: 13)),
+                        Text(detail[index]['date'],style: TextStyle(fontSize: 13)),
+                        Text(detail[index]['time'],style: TextStyle(fontSize: 13)),
+                        Text(detail[index]['place'],style: TextStyle(fontSize: 13)),
+                      ],
+                    ),
+                    Spacer(),
+                    Container(
+                      height: 30,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+
+                      child: Center(child: Text("Payment Success", style: TextStyle(fontSize: 10, color: Colors.white), )),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              );
+            },
+          );
         },
       ),
     );
